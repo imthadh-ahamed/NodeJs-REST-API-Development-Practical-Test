ac00fb08504bc7bd187c0efa3963d9e440d4611a
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-07 12:22:16 +0530

    Se the index.js file codes

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+// import postRoutes from "./routes/postRoutes.js";
+import chalk from "chalk";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+// app.use("/api/posts", postRoutes);
+
+
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(chalk.yellow("Connected to MongoDB"));
+    app.listen(PORT, () => {
+      console.log(chalk.yellow(`Server is running on port ${PORT}`));
+    });
+  })
+  .catch((err) => {
+    console.error(chalk.red("Failed to connect to MongoDB"), err);
+  });
